b3b33095a8f2bcc22c17688d43ad5e059b215384
Ubuntu <ubuntu@ip-172-31-41-20.ec2.internal>
2019-12-13 09:54:26 +0000

    Commit

--- src/main/java/com/cts/design/DesignPattern/DAO/TabletDAO.java
+++ src/main/java/com/cts/design/DesignPattern/DAO/TabletDAO.java
@@ -5,20 +5,21 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-
-
-public class TabletDAO implements TabletDAOInterface{
+public class TabletDAO implements TabletDAOInterface {
 	static Set<Tablet> tablets;
 	static Tablet obj;
+
 	@Override
-	public  void createTablet(Tablet tablet) {
+	public void createTablet(Tablet tablet) {
 		tablets.add(tablet);
-		String data=tablet.getTabletName()+","+tablet.getManufacturer()+","+tablet.getManufactureDate().getDayOfMonth()+"/"+tablet.getManufactureDate().getMonthValue()+"/"+tablet.getManufactureDate().getYear()+","+tablet.getExpiryDate().getDayOfMonth()+"/"+tablet.getExpiryDate().getMonthValue()+"/"+tablet.getExpiryDate().getYear();
+		String data = "\n"+tablet.getTabletName() + "," + tablet.getManufacturer() + ","
+				+ tablet.getManufactureDate().getDayOfMonth() + "/" + tablet.getManufactureDate().getMonthValue() + "/"
+				+ tablet.getManufactureDate().getYear() + "," + tablet.getExpiryDate().getDayOfMonth() + "/"
+				+ tablet.getExpiryDate().getMonthValue() + "/" + tablet.getExpiryDate().getYear();
 		try {
 			Files.write(Paths.get("src/data/tablet_details.txt"), data.getBytes(), StandardOpenOption.APPEND);
 		} catch (IOException e) {
@@ -28,7 +29,8 @@ public class TabletDAO implements TabletDAOInterface{
 
 	@Override
 	public void deleteTablet(String tabletName) {
-		Optional<Tablet> remTab=tablets.stream().filter(t->t.getTabletName().equalsIgnoreCase(tabletName)).findFirst();
+		Optional<Tablet> remTab = tablets.stream().filter(t -> t.getTabletName().equalsIgnoreCase(tabletName))
+				.findFirst();
 		tablets.remove(remTab.get());
 		try {
 			Files.deleteIfExists(Paths.get("src/data/tablet_details.txt"));
@@ -37,17 +39,22 @@ public class TabletDAO implements TabletDAOInterface{
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
-		tablets.stream().forEach(t->{
+
+		tablets.stream().forEach(t -> {
 			try {
-				Files.write((Paths.get("src/data/tablet_details.txt")), (t.getTabletName()+","+t.getManufacturer()+","+t.getManufactureDate().getDayOfMonth()+"/"+t.getManufactureDate().getMonthValue()+"/"+t.getManufactureDate().getYear()+","+t.getExpiryDate().getDayOfMonth()+"/"+t.getExpiryDate().getMonthValue()+"/"+t.getExpiryDate().getYear()+"\n").getBytes(),StandardOpenOption.APPEND);
+				Files.write((Paths.get("src/data/tablet_details.txt")),
+						(t.getTabletName() + "," + t.getManufacturer() + "," + t.getManufactureDate().getDayOfMonth()
+								+ "/" + t.getManufactureDate().getMonthValue() + "/" + t.getManufactureDate().getYear()
+								+ "," + t.getExpiryDate().getDayOfMonth() + "/" + t.getExpiryDate().getMonthValue()
+								+ "/" + t.getExpiryDate().getYear() + "\n").getBytes(),
+						StandardOpenOption.APPEND);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		});
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -60,45 +67,53 @@ public class TabletDAO implements TabletDAOInterface{
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
-		tablets.stream().forEach(t->{
+
+		tablets.stream().forEach(t -> {
 			try {
-				Files.write((Paths.get("src/data/tablet_details.txt")), (t.getTabletName()+","+t.getManufacturer()+","+t.getManufactureDate().getDayOfMonth()+"/"+t.getManufactureDate().getMonthValue()+"/"+t.getManufactureDate().getYear()+","+t.getExpiryDate().getDayOfMonth()+"/"+t.getExpiryDate().getMonthValue()+"/"+t.getExpiryDate().getYear()+"\n").getBytes(),StandardOpenOption.APPEND);
+				Files.write((Paths.get("src/data/tablet_details.txt")),
+						(t.getTabletName() + "," + t.getManufacturer() + "," + t.getManufactureDate().getDayOfMonth()
+								+ "/" + t.getManufactureDate().getMonthValue() + "/" + t.getManufactureDate().getYear()
+								+ "," + t.getExpiryDate().getDayOfMonth() + "/" + t.getExpiryDate().getMonthValue()
+								+ "/" + t.getExpiryDate().getYear() + "\n").getBytes(),
+						StandardOpenOption.APPEND);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		});
-		
+
 	}
 
 	@Override
 	public void searchTablet(String tabletName) {
-		tablets.stream().filter(t->t.getTabletName().equalsIgnoreCase(tabletName)).forEach(System.out::println);
-		
+		tablets.stream().filter(t -> t.getTabletName().equalsIgnoreCase(tabletName)).forEach(System.out::println);
+
 	}
-	public  static void getTabletsFromFile() {
-		
-			 try {
-				 tablets= Files.lines(Paths.get("src/data/tablet_details.txt")).map(Utility::parseTablet)
-						 //.filter(issue ->issue!=null && issue.getIssueId().startsWith("IS"))
-						 .collect(Collectors.toSet());
-			 }catch(IOException e) {
-				 e.printStackTrace();
-			 }
-		
+
+	public static void getTabletsFromFile() {
+
+		try {
+			tablets = Files.lines(Paths.get("src/data/tablet_details.txt")).map(Utility::parseTablet)
+					.filter(t ->t.getTabletName()!=null)
+					// issue.getIssueId().startsWith("IS"))
+					.collect(Collectors.toSet());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
+
 	public static void main(String[] args) {
 		getTabletsFromFile();
 		//tablets.forEach(System.out::println);
-		TabletDAO obj=new TabletDAO();
-		//obj.createTablet(new Tablet("Amitwas","ABC",LocalDate.of(2010, 06, 23),LocalDate.of(2010, 07, 23)));
+		TabletDAO obj = new TabletDAO();
+		obj.createTablet(new Tablet("Amitwas","ABC",LocalDate.of(2010, 06, 23),LocalDate.of(2010, 07, 23)));
 		//tablets.forEach(System.out::println);
-		//obj.deleteTablet("amitwa");
+		obj.deleteTablet("amitwas");
 		//tablets.forEach(System.out::println);
 		obj.searchTablet("pentaids1");
-		//tablets.forEach(System.out::println);
-		obj.updateTablet(new Tablet("Amit","ABCD",LocalDate.of(2010, 06, 23),LocalDate.of(2010, 07, 23)));
+		// tablets.forEach(System.out::println);
+		obj.updateTablet(new Tablet("Amit", "ABCD", LocalDate.of(2010, 06, 23), LocalDate.of(2010, 07, 23)));
 		tablets.forEach(System.out::println);
 	}
 
